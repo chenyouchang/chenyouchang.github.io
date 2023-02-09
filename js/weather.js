1101b6d357a2af910077138a8a8078eac794250d
chenyouchang <1026597164@qq.com>
2023-02-09 14:28:42 +0000

    软件安装汇总 UTC 2023-02-09 Thursday 14:28:42 Updated By Github Actions

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "1",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "zh",
+        "borderRadius": "5",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "250",
+        "top": "10",
+        "key": "82f8ee4c93e348acb07d3ca2e6ef7168"
+    }
+}
